02fa34775f5ab13e10be4919929ee4546b7e5846
Marcin Zadrzynski <warshipdesign@gmail.com>
2022-05-27 15:47:07 +0200

    autohandel 27.05

--- Python_Data/porownanie.py
+++ Python_Data/porownanie.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #df = (pd.read_excel("OA - Porownanie_20220420_1220.xlsx", "Export Worksheet", usecols="G, H"))
-df = (pd.read_excel("doors.xlsx", "Export Worksheet"))
+df = (pd.read_excel("num_funcki.xlsx", "Export Worksheet"))
 
 #print/read rows
 #print(df.head(5))
@@ -17,19 +17,18 @@ df = (pd.read_excel("doors.xlsx", "Export Worksheet"))
 #print(df.iloc[2, 6])
 
 #wpisuje wszystkie wyrazy z komórki excela w tablice i porównuje je z drugą
-for i in range(0, 1483):
+for i in range(0, 42):
     try:
-        a = df.iloc[i, 10]
-        b = df.iloc[i, 13]
+        a = df.iloc[i, 7]
+        b = df.iloc[i, 8]
         x = a.split()
         y = b.split()
 
         data_1 = {'TYPE': x}
         data_2 = {'TYPE': y} 
         if np.array_equal(data_1, data_2):
-            df.iloc[i, 14] = 'jest ok'
+            df.iloc[i, 9] = 'jest ok'
     except:
+            #df.iloc[i, 9] = 'jest zle'
             print(i)
-            #df.iloc[i, 14] = 'jest zle'
-    
-df.to_excel("doors2.xlsx", sheet_name="Sheet3")
+df.to_excel("test2.xlsx", sheet_name="Sheet3")
---
+++ autohandel/src/com/company/Owner.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Owner {
+    public String name;
+    public Integer cash;
+    public Car car;
+}
